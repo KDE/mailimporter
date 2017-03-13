582513ec1682f70315ea2c3c6148a2c63caf4205
Montel Laurent <montel@kde.org>
2017-03-13 13:41:05 +0100

    Prepare to move core file in specific directory

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 $EXTRACTRC `find . -name '*.ui'` >> rc.cpp || exit 11
-$XGETTEXT *.cpp -o $podir/libmailimporter.pot
+$XGETTEXT `find . -name '*.cpp' | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/libmailimporter.pot
 rm -f rc.cpp
