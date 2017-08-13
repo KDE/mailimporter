ceb35ebfa685c067ca0d648e6d2ba1b49b1f3c1c
Allen Winter <winter@kde.org>
2017-08-13 15:07:13 -0400

    filtermailapp.h - add GPLv2+ license, to match that in filtermappapp.cpp same author and same date as the .cpp

--- src/mailimporter/filters/filtermailapp.h
+++ src/mailimporter/filters/filtermailapp.h
@@ -10,6 +10,15 @@
 
  ***************************************************************************/
 
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef MAILIMPORTER_FILTER_MAILAPP_H
 #define MAILIMPORTER_FILTER_MAILAPP_H
 
