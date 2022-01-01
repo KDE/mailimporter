7ff25d6bd1ed153cd86c6b3bf84556b74e28faa0
Laurent Montel <montel@kde.org>
2022-01-01 16:16:42 +0100

    GIT_SILENT: happy new year

--- src/mailimporter/filters/filtersylpheed.cpp
+++ src/mailimporter/filters/filtersylpheed.cpp
@@ -2,7 +2,7 @@
   filtersylpheed.cpp  -  Sylpheed maildir mail import
 
   SPDX-FileCopyrightText: 2005 Danny Kukawka <danny.kukawka@web.de>
-  SPDX-FileCopyrightText: 2012-2021 Laurent Montel <montel@kde.org>
+  SPDX-FileCopyrightText: 2012-2022 Laurent Montel <montel@kde.org>
 
   SPDX-License-Identifier: GPL-2.0-or-later
 */
