1988ff73080b54b7bbea6362a19b7b56a606b579
Laurent Montel <montel@kde.org>
2023-12-23 08:14:56 +0100

    Fix the translation template version number (add 6 for kf6)
    Make sure that it will not conflict with qt5 version.
    
    @teams/localization

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 $EXTRACTRC `find . -name '*.ui'` >> rc.cpp || exit 11
-$XGETTEXT `find . -name '*.cpp' | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/libmailimporter.pot
+$XGETTEXT `find . -name '*.cpp' | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/libmailimporter6.pot
 rm -f rc.cpp
